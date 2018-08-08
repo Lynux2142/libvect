515e45b1b8146a7498ff3dcc35e6e6ba8b3ab17e
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-08-08 17:34:36 +0200

    ++

---
+++ libvect.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libvect.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/26 15:36:45 by lguiller          #+#    #+#             */
+/*   Updated: 2018/08/08 17:18:15 by lguiller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBVECT_H
+# define LIBVECT_H
+
+# include <math.h>
+
+typedef struct		s_mat3
+{
+	double			x;
+	double			y;
+	double			z;
+}					t_mat3;
+
+t_mat3				ft_rot_x(t_mat3 v, double a);
+t_mat3				ft_rot_y(t_mat3 v, double a);
+t_mat3				ft_rot_z(t_mat3 v, double a);
+t_mat3				ft_normalize(t_mat3 v);
+double				ft_vect_dist(t_mat3 v);
+
+#endif
